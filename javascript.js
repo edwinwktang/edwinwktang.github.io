fe3bd5421c63586a980af2a6837f666d33fe10d4
edwinwktang <edwinwk.tang@gmail.com>
2024-01-03 17:59:44 -0800

    Added project filters

--- javascript.js
+++ javascript.js
@@ -20,9 +20,42 @@ function setLightTheme() {
 
 // Immediately invoked function to set the theme on initial load
 (function () {
-   if (localStorage.getItem('theme') === 'theme-dark') {
-       setTheme('theme-dark');
-   } else {
-       setTheme('theme-light');
-   }
-})();
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-dark');
+    } else {
+        setTheme('theme-light');
+    }
+ })();
+
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("project-filter-link");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "project-filter-show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "project-filter-show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+
